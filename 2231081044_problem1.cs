65cae2abe5a92c6d47716a3eda351e6c183e8ba0
Yjrjoy <160854170+Yjrjoy@users.noreply.github.com>
2024-02-22 20:31:43 +0600

    Add files via upload

---
+++ 2231081044_problem1.cs
@@ -0,0 +1,23 @@
+
+using System;
+
+namespace Hoss
+{
+public static class Program 
+{         
+    public static void Main(string[] args)
+    {
+          //UU_2231081044, Md. Murad Hossain
+	    //input
+		int a,b;
+		Console.WriteLine("Enter two number : ");
+		a = Convert.ToInt32(Console.ReadLine());
+		b = Convert.ToInt32(Console.ReadLine());
+		//output
+		Console.WriteLine("{0}+{1}={2}",a,b,a+b);
+		Console.WriteLine("{0}-{1}={2}",a,b,a-b);
+		Console.WriteLine("{0}*{1}={2}",a,b,a*b);
+		Console.WriteLine("{0}/{1}={2}",a,b,a/b);
+    }
+ }
+}
